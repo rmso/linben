5b542a2269b63ffe3241ea98a3bf408536b54745
Raquel Oliveira <rmso@cin.ufpe.br>
2017-12-06 20:07:30 -0300

    Rotas do mapa

--- app/src/main/java/com/example/samsung/linben/views/HemocentroActivity.java
+++ app/src/main/java/com/example/samsung/linben/views/HemocentroActivity.java
@@ -1,33 +1,61 @@
 package com.example.samsung.linben.views;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
 import com.example.samsung.linben.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Raquel on 06/07/2016.
  */
-public class HemocentroActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class HemocentroActivity extends AppCompatActivity implements OnMapReadyCallback, ConnectionCallbacks, OnConnectionFailedListener,
+        LocationListener, RoutingListener  {
 
     private Button btn_verRota;
     private TextView tv_link;
     private GoogleMap mMap;
     LatLng latLng;
+    LatLng latLngHemope;
+    GoogleApiClient mGoogleApiClient;
+    private List<Polyline> polylines;
+
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
 
 
@@ -36,21 +64,22 @@ public class HemocentroActivity extends AppCompatActivity implements OnMapReadyC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hemocentro);
 
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
-        }
-
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment;
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        btn_verRota = (Button) findViewById(R.id.btn_ver_rota);
-        tv_link = (TextView) findViewById(R.id.tv_link);
+        polylines = new ArrayList<>();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        latLng = null;
+
+        btn_verRota = findViewById(R.id.btn_ver_rota);
+        tv_link = findViewById(R.id.tv_link);
 
         tv_link.setOnClickListener(new View.OnClickListener() {
 
@@ -60,24 +89,79 @@ public class HemocentroActivity extends AppCompatActivity implements OnMapReadyC
                 startActivity(intent);
             }
         });
+
+        btn_verRota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopLocationUpdates();
+                mMap.clear();
+
+                CameraPosition position = new CameraPosition.Builder().target(latLngHemope).zoom(12).build();
+                CameraUpdate update = CameraUpdateFactory.newCameraPosition(position);
+
+                mMap.addMarker(new MarkerOptions().position(latLngHemope).title("Hemope"));
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Você").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+                Routing routing = new Routing.Builder()
+                        .travelMode(Routing.TravelMode.DRIVING)
+                        .withListener(HemocentroActivity.this)
+                        .alternativeRoutes(false)
+                        .waypoints(latLng, latLngHemope)
+                        .build();
+                routing.execute();
+
+                mMap.animateCamera(update, 1000, new GoogleMap.CancelableCallback() {
+                    @Override
+                    public void onFinish() {
+                    }
+
+                    @Override
+                    public void onCancel() {
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
 
-        latLng = new LatLng(-8.05428, -34.8813);
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        latLng = new LatLng(latitude, longitude);
+        mMap.clear();
+        latLngHemope = new LatLng(-8.0527208, -34.8979324);
 
         mMap.clear();
 
-        CameraPosition position = new CameraPosition.Builder().target(latLng).zoom(12).build();
+        CameraPosition position = new CameraPosition.Builder().target(latLngHemope).zoom(17).build();
         CameraUpdate update = CameraUpdateFactory.newCameraPosition(position);
 
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng).title("Position");
+        markerOptions.position(latLngHemope)
+                .title("Position");
 
-        Marker marker = mMap.addMarker(markerOptions);
-        marker.setPosition(latLng);
+        mMap.addMarker(new MarkerOptions().position(latLngHemope).title("Hemope"));
 
         mMap.animateCamera(update, 1000, new GoogleMap.CancelableCallback() {
             @Override
@@ -89,4 +173,93 @@ public class HemocentroActivity extends AppCompatActivity implements OnMapReadyC
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+        }
+
+    @Override
+    protected void onStop() {
+        stopLocationUpdates();
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    protected void startLocationUpdates() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(100);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        } else {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+
+    }
+
+    @Override
+    public void onRoutingStart() {
+
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> route, int i) {
+        if(polylines.size()>0) {
+            for (Polyline poly : polylines) {
+                poly.remove();
+            }
+        }
+        polylines = new ArrayList<>();
+        for (i = 0; i <route.size(); i++) {
+            PolylineOptions polyOptions = new PolylineOptions();
+            polyOptions.getColor();
+            polyOptions.width(10 + i * 3);
+            polyOptions.addAll(route.get(i).getPoints());
+            Polyline polyline = mMap.addPolyline(polyOptions);
+            polylines.add(polyline);
+        }
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    int grantResult = grantResults[i];
+
+                    if (permission.equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                            if(mGoogleApiClient.isConnected()) {
+                                startLocationUpdates();
+                            }
+                        } else {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
