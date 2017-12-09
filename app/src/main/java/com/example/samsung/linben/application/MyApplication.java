915529f63ec86df327c1855a1f7ac1e7737e42ff
barbaraclo <barbaraclima.o@gmail.com>
2017-12-09 18:48:04 -0300

    Adicionando dependencias do LeakCanary

---
+++ app/src/main/java/com/example/samsung/linben/application/MyApplication.java
@@ -0,0 +1,24 @@
+package com.example.samsung.linben.application;
+
+import android.app.Application;
+import com.squareup.leakcanary.LeakCanary;
+
+
+/**
+ * Created by Samsung on 09/12/2017.
+ */
+
+public class MyApplication extends Application{
+
+    @Override public void onCreate() {
+        super.onCreate();
+        //LeakCanary
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+    }
+}
